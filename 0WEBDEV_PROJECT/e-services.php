9c5004073a560920c233138ad35084c0c68a921a
kiwi098 <83574695+kiwi098@users.noreply.github.com>
2023-11-04 04:06:36 +0800

    All Files

---
+++ 0WEBDEV_PROJECT/e-services.php
@@ -0,0 +1,465 @@
+<html>
+<!--
+    GROUP 2:
+        MORFE, Tristan Michael V. 
+        PAULY, Leila Joyce H.
+        SAMONTE, Eina Danielle T.
+        Villanueva, Blessy Grace D. 
+        BSCS 3-2
+-->
+<head>
+    <title>
+        Barangay 3-2 | E-Services
+    </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+
+    <link href = "head_design.css" type = "text/css" rel = "stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <!-- FOOTER ICONS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/35200fcd01.js" crossorigin="anonymous"></script>
+
+    <style>
+        #eservices_button {
+            background-color: #cfb578;
+            }
+
+            .eservices_content {
+            background-color: #cfb578;
+            text-align: center;
+            }
+
+            /* C28F48*/
+
+
+            #home_color {
+            color: black;
+            }
+
+            #aboutus_color {
+            color: black;
+            }
+
+            #eservices_color {
+            color: white;
+            }
+
+            #home_button:hover{
+            background-color: #3D677B;
+            }
+
+            #aboutus_button:hover{
+            background-color: #6CC1AB;
+            }
+            
+            .intro_text{
+                padding-left: 50px;
+                padding-right: 50px;
+                text-align:center;
+                font-family: 'Merriweather', serif;
+                color: #74480A;
+            }
+
+            .form_1{
+                margin: auto;
+                /*position: absolute;
+                left: 151px;
+                top: 490px;*/
+
+            }
+
+            
+        #home_button, #aboutus_button, #eservices_button, #login{
+            font-family: 'Merriweather', serif;
+        }
+        #eservices_button {
+        background-color: #cfb578;
+        }
+
+        .eservices_content {
+        background-color: #cfb578;
+        text-align: center;
+        }
+        #aboutus_button:hover{
+        background-color: #6CC1AB;
+        }
+
+        /* FOOTER DESIGN */
+        
+        .footer {
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+            background-color: #eff1f1;
+        }
+        .details_lbl {
+            padding-left: 135px;
+            text-align: left;
+            font-size: 25px;
+            font-family: 'Open Sans', sans-serif;
+            opacity: 0.8;
+            letter-spacing: 5px;
+        }
+        .about_lbl {
+            font-size: 25px;
+            text-align: right;
+            padding-right: 210px;
+            opacity: 0.8;
+            font-family: 'Open Sans', sans-serif;
+            letter-spacing: 5px;
+        }
+        .location {
+            text-align: left;
+            padding-left: 100px;
+            opacity: 0.8;
+            height: 40px;
+        }
+        .location_lbl {
+            padding-left: 20px;
+            font-family: 'Open Sans', sans-serif;
+            opacity: 0.8;
+        }
+        .phone {
+            text-align: left;
+            opacity: 0.8;
+            height: 40px;
+            padding-left: 100px;
+        }
+        .phone_lbl {
+            padding-left: 10px;
+            font-family: 'Open Sans', sans-serif;
+            opacity: 0.8;
+        }
+        .email {
+            text-align: left;
+            opacity: 0.8;
+            height: 40px;
+            padding-left: 100px;
+        }
+        .email_lbl {
+            padding-left: 15px;
+            font-family: 'Open Sans', sans-serif;
+            opacity: 0.8;
+        }
+
+
+        
+        .social_media {
+            text-align: right;
+            /* padding-right: 200px; */
+            padding-right: 150px;
+            letter-spacing: 15px;
+            opacity: 0.8;
+        }
+
+        .about_details {
+            text-align: right;
+            font-family: 'Open Sans', sans-serif;
+            opacity: 0.8;
+            padding-right: 100px;
+        }
+        .copyright {
+            padding-top: 15px;
+            padding-bottom: 15px;
+            background-color: gray;
+            color: black;
+            text-align: center;
+            opacity: 0.8;
+            font-family: 'Open Sans', sans-serif;
+        }
+        p {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        .container 
+        {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        .container .box 
+        {
+            position: relative;
+            width: 350px;
+            height: 480px;
+            padding: 10px;
+            background: #fff;
+            box-shadow: 0 5px 15px rgba(0,0,0,.1);
+            border-radius: 4px;
+            margin: 0 auto;
+            box-sizing: border-box;
+            overflow: hidden;
+            text-align: center;
+        }
+
+        .container .box:before 
+        {
+            content: '';
+            width: 50%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background: rgba(255,255,255,.2);
+            z-index: 2;
+            pointer-events: none;
+        }
+
+        .container .box .icon
+        {
+            position: relative;
+            width: 80px;
+            height: 80px;
+            color: #74480A;
+            background: #e9d29e;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+            border-radius: 50%;
+            font-size: 40px;
+            font-weight: 700;
+            transition: 1s;
+        }
+
+        .container .box:nth-child(1) .icon
+        {
+            box-shadow: 0 0 0 0 #e9d29e;
+            background: #e9d29e;
+        }
+
+        .container .box:nth-child(1):hover .icon
+        {
+            box-shadow: 0 0 0 400px #e9d29e;
+        }
+
+        .container .box:nth-child(2) .icon
+        {
+            box-shadow: 0 0 0 0 #e9d29e;
+            background: #e9d29e;
+        }
+
+        .container .box:nth-child(2):hover .icon
+        {
+            box-shadow: 0 0 0 400px #e9d29e;
+        }
+        l
+        .container .box:nth-child(3) .icon
+        {
+            box-shadow: 0 0 0 0 #e9d29e;
+            background: #e9d29e;
+        }
+
+        .container .box:nth-child(3):hover .icon
+        {
+            box-shadow: 0 0 0 400px #e9d29e;
+        }
+
+
+        .container .box .content
+        {
+            position: relative;
+            z-index: 1;
+            transition: 0.5s;
+
+        }
+
+        .container .box:hover .content
+        {
+            color: #ffffff;
+        }
+
+
+        .container .box .content h3
+        {
+            font-size: 20px;
+            margin: 0px 0px;
+            padding: 0;
+            justify-content: center;
+        }
+
+        .container .box .content p
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container .box .content img
+        {
+            width: 300px;
+            height: 300px;
+        }
+
+        #apply
+        {
+            display: inline-block;
+            padding: 10px 60px;
+            background: #f5f5dc;
+            border-radius: 20px;
+            text-decoration: none;
+            color: #74480A;
+            font-weight: 500;
+            margin-top: 20px;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
+            
+        }
+        .container .box:hover .content #apply
+        {
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
+        }
+
+        /* DROPDOWN */
+        .dropdown {
+            float: right;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: inherit;  
+            border: none;
+            outline: none;
+            color: black;
+            padding-top: 14px;
+            padding-right: 14px;
+            padding-left: 8px;
+            background-color: inherit;
+            font-family: inherit;
+            margin: 0;
+        }
+
+        .dropbtn:hover{
+            background-color: #dad3c2;
+        }
+        
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #dad3c2;
+            min-width: 160px;
+            box-shadow: 0px 0px 16px 8px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+
+    </style>
+
+</head>
+
+<body>
+
+<body>
+
+<!-- HEADERS -->
+<div>
+    <img id = "header_img" src = "/img/header.jpg" alt = 'header.jpg'> 
+</div>
+<!-- TABS -->
+<table id = header_table>
+    <tr>
+        <th id = "home_button"><a id="home_color" href = "homepage.php">HOME</a></th>
+        <th id = "aboutus_button"><a id="aboutus_color" href = "aboutus.php">ABOUT US</a></th>
+        <th id = "eservices_button"><a id="eservices_color" href = "e-services.php">E-SERVICES</a></th>
+        <th id = blank>--------------------------------</th>
+        <th id = "login">
+            <div class="dropdown">
+                <button class="dropbtn">ACCOUNT <i class="fa fa-caret-down"></i></button>
+                <div class="dropdown-content">
+                    <a href="AdminLoginSystem.php">Admin</a>
+                    <a href="ResidentLoginSystem.php">Resident</a>
+                </div>
+            </div>    
+        </th>
+    </tr>
+</table>
+
+<div class = "eservices_content">
+    <br>
+    <div class = intro_page>
+    
+    <h2 class="intro_text">
+        Barangay 3-2 offers three electronic services to help K- Nearest Neighbors minimize physical contact:
+    </h2>
+    </div>
+    <br>
+    <br>
+    <div class = "container">
+	<div class = "box">
+		<div class = "icon">01</div>
+		<div class = "content">
+			<h3> <img src="/img/form.png"> </h3>
+			
+				<a id=apply href = "brgy_clearance.php">APPLY</a>
+		</div>     
+	</div> 
+	<div class = "box">
+		<div class = "icon">02</div>
+		<div class = "content">
+			<h3> <img src="/img/form1.png"> </h3>
+				<a id=apply href = "business_clearance.php">APPLY</a>
+		</div>     
+	</div> 
+	<div class = "box">
+		<div class = "icon">03</div>
+		<div class = "content">
+			<h3> <img src="/img/form2.png"> </h3>
+				<a id=apply href = "certificate_residency.php">APPLY</a>
+		</div>     
+	</div> 
+</div>
+    <br>
+    <br>
+    <br>
+</div>
+
+<table class = "footer"> 
+    <tr>
+        <th class = "details_lbl" style = "padding-top: 25px;"> DETAILS </th>
+        <th class = "about_lbl" style = "padding-top: 25px;"> ABOUT </th>
+    </tr>
+
+    <tr>    
+        <td class = "location"><i class="fas fa-map-marker-alt" style="font-size:25px;"></i><a class = "location_lbl">Eh Di Sa Puso Mo</a> </td>
+        <td class = "about_details">This is the official website of Barangay 3-2 </td>
+    </tr>
+
+    <tr>     
+        <td class = "phone"><i class="fas fa-phone-alt" style="font-size:25px;"></i><a class = "phone_lbl"> +63-999-143-9999 </a> </td>
+        <td class = "social_media"> <i class="fab fa-facebook-square" style = "font-size: 50px;"></i> <i class="fab fa-twitter-square" style = "font-size: 50px;"></i> <i class="fab fa-instagram-square" style = "font-size: 50px;"></i> </td>
+    </tr>
+
+    <tr>    
+        <td class = "email" style = "padding-bottom: 25px;"><i class="fas fa-envelope" style="font-size:25px; "></i><a class = "email_lbl">example123@gmail.com</a> </td>
+        
+    </tr>
+</table>
+    <p class = "copyright"> Barangay 3-2 | Copyright &copy; 2021</p>
+
+
+</body>
+</html>
+
